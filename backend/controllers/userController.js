fe76c553b16fb8c1cf7ab035a86020f26cd8de90
nasimimran <nasimuzzamanimran@gmail.com>
2023-10-07 21:52:12 +0600

    user login create

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,3 +23,34 @@ exports.registerUser = catchAsyncError(async (req,res,next)=>{
         token,
     });
 });
+
+// Login user
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+    const {email, password} = req.body;
+
+    // checking user has given password and email both
+
+    if (!email || !password) {
+        return next(new ErrorHandler("please provide a valid email & password",400));
+    }
+
+    const user = await User.findOne({email}).select("+password");
+
+    if (!user) {
+        return next(new ErrorHandler("Invalid email or password",401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid email or password",401));
+    }
+
+    const token = user.getJWTToken();
+
+    res.status(200).json({
+        success: true,
+        token,
+    });
+
+});
