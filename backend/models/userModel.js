fbcdae54a49e7f5c15bdb5a7b3f0bb76d22a6c39
nasimimran <nasimuzzamanimran@gmail.com>
2023-10-07 18:46:45 +0600

    user password incrypted

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -38,6 +39,16 @@ const userSchema = new mongoose.Schema({
     
     resetePasswordToken: String,
     resetePasswordExpire: Date,
+});
+
+userSchema.pre('save', async function (next) {
+
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    this.password = await bcrypt.hash(this.password,10);
+
 });
 
 module.exports = mongoose.model("User", userSchema);
