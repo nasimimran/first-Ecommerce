07a2d313974f56f666a63ff9c51b47ae975e3eb6
nasimimran <nasimuzzamanimran@gmail.com>
2023-10-07 14:32:08 +0600

    userController and userRoute created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncError = require("../middleware/catchAsyncErrors");
+const User = require("../models/userModel");
+
+// Register a User
+exports.registerUser = catchAsyncError(async (req,res,next)=>{
+    const {name, email, password } = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: "this is a sample id",
+            url: "profilepicUrl",
+        },
+    });
+
+    res.status(201).json({
+        success: true,
+        user,
+    });
+});
