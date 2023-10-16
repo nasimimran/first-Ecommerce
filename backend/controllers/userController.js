5768c6a59733af14cac006603e757fa4eaac3688
nasimimran <nasimuzzamanimran@gmail.com>
2023-10-16 15:38:48 +0600

    update profile funtion added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -162,3 +162,23 @@ exports.updatePassword = catchAsyncError ( async (req, res, next) => {
 
     sendToken(user, 200, res);
 });
+
+// update user profile
+exports.updateProfile = catchAsyncError ( async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email
+    };
+
+    // we will add cloudinary later
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true
+    });
+});
