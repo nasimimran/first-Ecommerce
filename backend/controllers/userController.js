a1c3b0dc1d5e1d385adf7ede96493ede6b5e1634
nasimimran <nasimuzzamanimran@gmail.com>
2023-10-16 14:56:15 +0600

    get user details funtion added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -129,3 +129,14 @@ exports.resetPassword = catchAsyncError(async (req, res, next) => {
 
     sendToken(user, 200, res);
 });
+
+// Get user details
+exports.getUserDetails = catchAsyncError ( async (req, res, next) => {
+
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const { registerUser, loginUser, logout, forgotPassword, resetPassword} = require("../controllers/userController");
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails} = require("../controllers/userController");
+const {isAuthenticatedUser, authorizeRoles} = require("../middleware/outh");
+
 const router = express.Router();
+
 
 router.route("/register").post(registerUser);
 
@@ -11,5 +14,7 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
