bac44575880513a6e62f10907f5a78907af14ffb
nasimimran <nasimuzzamanimran@gmail.com>
2023-11-17 18:55:31 +0600

    Get all reviews for a product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -132,3 +132,18 @@ exports.createProductReview = catchAsyncError ( async (req, res, next) => {
         success: true,
     });
 });
+
+// Get all reviews for a product
+exports.getProductReviews = catchAsyncError ( async (req, res, next) => {
+
+    const product = await Product.findById(req.query.id);
+
+    if (!product){
+        return next(new ErrorHandler("Product not found",404));
+    }
+
+    res.status(200).json({
+        success: true,
+        reviews: product.reviews
+    });
+});
