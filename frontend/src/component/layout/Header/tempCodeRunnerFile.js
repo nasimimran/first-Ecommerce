c5f1e019cd0413b09ede7a259d86660ed643ca2c
nasimimran <nasimuzzamanimran@gmail.com>
2024-01-20 16:02:12 +0600

    new navbar create and old navbar deleted

--- frontend/src/component/layout/Header/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-import SearchIconElement from "../../../../src/images/search.png";
-import CartIconElement from "../../../../src/images/cart.png";
-import ProfileIconElement from "../../../../src/images/profile.png";
