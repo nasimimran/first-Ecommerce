44c0cabb0f9fd5cbf9bef8404c6beff020567e55
nasimimran <nasimuzzamanimran@gmail.com>
2024-01-26 15:13:57 +0600

    Search rout for frontend is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import WebFont from 'webfontloader';
 import Home from './component/Home/Home.js';
 import ProductDetails from "./component/Product/ProductDetails.js";
 import Products from "./component/Product/Products.js";
+import Search from "./component/Product/Search.js";
 
 function App() {
   React.useEffect(() => {
@@ -27,6 +28,8 @@ function App() {
         <Route exact path="/" Component={Home} />
         <Route exact path="/product/:id" Component={ProductDetails} />
         <Route exact path="/products" Component={Products} />
+        <Route exact path="/search" Component={Search} />
+      
       </Routes>
 
       <Footer />
---
+++ frontend/src/component/Product/Search.js
@@ -0,0 +1,33 @@
+import React, { useState, Fragment} from 'react';
+import { useNavigate } from "react-router-dom";
+import "./Search.css";
+
+
+const Search = () => {
+  const navigate = useNavigate();
+  const [keyword, setKeyword] = useState("");
+
+  const searchSubmitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      navigate(`/products/${keyword}`);
+    } else {
+      navigate("/products");
+    }
+  };
+
+  return (
+    <Fragment>
+      <form className='searchBox' onSubmit={searchSubmitHandler}>
+        <input
+          type='text'
+          placeholder='Search a product'
+          onChange={(e) => setKeyword(e.target.value)} 
+        />
+        <input type='submit' value="Search"/>
+      </form>
+    </Fragment>
+  )
+}
+
+export default Search;
